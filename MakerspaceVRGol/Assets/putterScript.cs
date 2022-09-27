a85e1fc90b8863ff32469c39bf6bc084e1594862
isaac <isaac@2pisoftware.com>
2022-09-27 15:58:34 +1000

    two handed grip and length wip

--- MakerspaceVRGol/Assets/putterScript.cs
+++ MakerspaceVRGol/Assets/putterScript.cs
@@ -20,9 +20,35 @@ public class putterScript : MonoBehaviour
     public GameObject rightController;
     private bool primaryButtonPressed = true;
 
+    private float _gripSample;
+    private float _gripStrength;
+    private bool _grabbingActive = false;
+
+    private Vector2 _thumbSample;
+    private float _thumbStrength;
+    private bool _thumbActive = false;
+
+    private bool _leftTriggerDown;
+    private bool _leftGripDown;
+    // and other left hand buttons
+
+    private bool _rightTriggerDown;
+    private bool _rightGripDown;
+    // and other right hand buttons
+
+    private bool isTwoHands = false;
+    public GameObject putter;
+    public Quaternion originalPutterRotation;
+    public float putterLengthSpeed;
+    public GameObject putterHead;
+    private Vector3 originalPutterHeadPos;
+
     // Start is called before the first frame update
     void Start()
     {
+        originalPutterHeadPos = putterHead.transform.localPosition;
+        originalPutterRotation = putter.transform.rotation;
+
         if (primaryButtonPress == null)
         {
             primaryButtonPress = new PrimaryButtonEvent();
@@ -30,7 +56,7 @@ public class putterScript : MonoBehaviour
 
         allDevices = new List<UnityEngine.XR.InputDevice>();
         devicesWithPrimaryButton = new List<UnityEngine.XR.InputDevice>();
-        InputTracking.nodeAdded += InputTracking_nodeAdded; ;
+        InputTracking.nodeAdded += InputTracking_nodeAdded;
 
     }
 
@@ -44,6 +70,11 @@ public class putterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _gripStrength = 0.0f;
+        _gripSample = 0.0f;
+        _thumbStrength = 0.0f;
+        _thumbSample = Vector2.zero;
+
         bool tempState = false;
         bool invalidDeviceFound = false;
         foreach (var device in devicesWithPrimaryButton)
@@ -56,6 +87,50 @@ public class putterScript : MonoBehaviour
 
             if (!device.isValid)
                 invalidDeviceFound = true;
+
+            device.TryGetFeatureValue(CommonUsages.grip, out _gripSample);
+            _gripStrength = Mathf.Max(_gripStrength, _gripSample);
+
+            device.TryGetFeatureValue(CommonUsages.primary2DAxis, out _thumbSample);
+            
+           //     .TryGetFeature Value
+           // _gripStrength = Mathf.Max(_gripStrength, _gripSample);
+        }
+
+        if (Mathf.Abs(_thumbSample.y) >= 0.2f)
+        {
+            putterHead.transform.localPosition = putterHead.transform.localPosition + (new Vector3(_thumbSample.y * putterLengthSpeed * Time.deltaTime, 0.0f, 0.0f));
+        }
+
+        if (_gripStrength > 0.2f)
+        {
+            if (!_grabbingActive)
+            {
+                _grabbingActive = true;
+                //press event
+                toggleTwoHands();
+            }
+
+
+        }
+        else if (_grabbingActive)
+        {
+            _grabbingActive = false;
+            //release event
+            toggleTwoHands();
+        }
+
+        if (_grabbingActive)
+        {
+            if (isLeftHand)
+            {
+                putter.transform.LookAt(rightController.transform);
+                //putter.transform.rot
+            }
+            else
+            {
+                putter.transform.LookAt(leftController.transform);
+            }
         }
 
         if (tempState != lastButtonState) // Button state changed since last frame
@@ -66,6 +141,10 @@ public class putterScript : MonoBehaviour
 
         if (invalidDeviceFound || devicesWithPrimaryButton.Count == 0) // refresh device lists
             updateInputDevices();
+
+
+      
+
     }
 
     // find any devices supporting the desired feature usage
@@ -81,6 +160,26 @@ public class putterScript : MonoBehaviour
             {
                 devicesWithPrimaryButton.Add(device); // Add any devices that have a primary button.
             }
+        }
+    }
+
+    void toggleTwoHands()
+    {
+        if (!isTwoHands)
+        {
+            if (isLeftHand)
+            {
+                putter.transform.LookAt(rightController.transform);
+               //putter.transform.rot
+            } else
+            {
+                putter.transform.LookAt(leftController.transform);
+            }
+            isTwoHands = true;
+        } else
+        {
+            putter.transform.localRotation = originalPutterRotation;
+            isTwoHands = false;
         }
     }
 
